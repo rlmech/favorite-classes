742a5acb316007a39de7097ab65fefaf89955a7c
Robert Lauv <robert.lauv3@gmail.com>
2022-05-23 22:09:17 -0700

    Added PeterPortal API, styled page

--- src/components/Class/ClassGraphQL.js
+++ src/components/Class/ClassGraphQL.js
@@ -1,28 +1,49 @@
 import React, { useEffect, useState } from "react";
 import "./Class.css";
 
-function Class(props){
+function ClassGraphQL(props){
 
     const[classInfo, setClassInfo] = useState({});
-    const url = "https://api.peterportal.org/rest/v0/courses/"
+    const url = "https://api.peterportal.org/graphql"
 
     useEffect(() => {
         const fetchData = async () => {
-            const response = await fetch(url + props.name)
+
+            const query = `
+                query{
+                    course(id:"${props.name}"){
+                        title
+                        department_name
+                        description
+                        instructor_history{
+                            name
+                        }
+                    }
+                }
+            `
+
+            const response = await fetch(url, {
+                method: "POST",
+                body: JSON.stringify({query}),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }, props.name)
             const data = await response.json();
             console.log(data)
-            setClassInfo(data);
+            setClassInfo(data.data.course);
         }
         fetchData();
     }, [props.name]);
+
     let info;
-    if(classInfo.id){
+    if(classInfo){
         info = <div className="information">
             <p className="title"> {classInfo.title} </p>
             <p className="department">{classInfo.department_name}</p>
             <p className="description">{classInfo.description}</p>
         </div>
-    } else if(classInfo.error) {
+    } else if(classInfo == null) {
         info = <p>Class Not Found</p>
     } else {
         info = <p>loading...</p>
@@ -38,4 +59,4 @@ function Class(props){
     )
 }
 
-export default Class;
+export default ClassGraphQL;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Class from "../Class/Class";
+import ClassGraphQL from "../Class/ClassGraphQL";
 import "./Home.css";
 
 function Home(props) {
@@ -22,15 +23,20 @@ function Home(props) {
 
     return (
         <div>
-            <h1>Hello World!</h1>
+            <h1>Poggers Classes</h1>
             <form onSubmit={handleSubmit}>
-                <label>Add Favorite Class</label>
-                <input type="text" value={value} onChange={handleChange}></input>
-                <button type="submit">Add Class</button>
+                <label>Add Favorite Class</label><br></br>
+                <input type="text" id="inputBox" placeholder="Class Here" value={value} onChange={handleChange}></input>
+                <button type="submit" id="submitButton">Add Class</button>
             </form>
-            <div className="my-classes">
+            {/* <div className="my-classes">
                 {favoriteClasses.map((favClass) =>
                     <Class name={favClass} key={favClass}></Class>
+                )}
+            </div> */}
+            <div className="my-classes">
+                {favoriteClasses.map((favClass) =>
+                    <ClassGraphQL name={favClass} key={favClass}></ClassGraphQL>
                 )}
             </div>
         </div>
